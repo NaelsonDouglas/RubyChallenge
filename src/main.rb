f9d9e57970c0d15601e66b7f2f21cac1c3c89f29
naelsondouglas <naelson17@gmail.com>
2019-10-23 19:02:38 -0300

    Added the code

---
+++ src/main.rb
@@ -0,0 +1,30 @@
+require 'time'
+def timeDiff(input_file)
+        content = File.read input_file
+        time_HHMM = /(\d{2}:\d{2})$/
+        hours = content.scan(time_HHMM)
+
+        start_time = Time.parse(hours.first.to_s)
+        end_time = Time.parse(hours.last.to_s)
+        
+        total_seconds = end_time-start_time
+        total_utc = Time.at(total_seconds).utc
+        total_str = total_utc.strftime("%H:%M")
+
+        puts input_file+": "+total_str+" hours"
+        return total_seconds
+end
+
+now = Time.now().utc
+sum = Time.now().utc
+
+puts "REPORT"
+for arg in ARGV        
+        sum +=  timeDiff arg                
+end
+
+elapset_time = (sum-now).floor
+
+elapsed_hours = (elapset_time/3600).floor
+elapsed_minutes = (elapset_time%3600).floor
+puts "total hours: "+elapsed_hours.to_s+":%02d"%[elapsed_minutes.to_s]+" hours"
